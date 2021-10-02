660b7d03db4ba23001cf4859af5a507fc90e7678
sonhakgyo <76437291+sonhakgyo@users.noreply.github.com>
2021-10-02 09:38:32 +0900

    update

---
+++ WebClass/JAVA/TestThread.java
@@ -0,0 +1,20 @@
+package WebClass.JAVA;
+
+public class TestThread extends Thread {
+    public static void main(String[] args) {
+        TestThread t1 = new TestThread();
+        // t1.start();
+    }
+
+    public void run(){
+        System.out.println("thread run.");
+    }
+
+    public void stop1(){
+      System.out.println("thred stop"); // t1.start() 메서드 확인 겸?
+    }
+
+    public void fly(){
+      System.out.println("thread fly"); // t1.start() 메서드 확인 겸?
+    }
+}
