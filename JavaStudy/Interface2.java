82472392ebc83fb4b24226799902f97aff9342aa
sonhakgyo <76437291+sonhakgyo@users.noreply.github.com>
2021-10-04 22:25:48 +0900

    update

---
+++ JavaStudy/Interface2.java
@@ -0,0 +1,24 @@
+package JavaStudy;
+
+public class Interface2 {
+    public static void main(String[] args) {
+      
+    }
+}
+interface Providable{
+    void leisureSports();
+    void sightseeing();
+    void food();
+}
+class KoreaTour implements Providable{
+    
+}
+  
+
+
+
+
+
+
+
+
