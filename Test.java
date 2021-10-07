5b84915e2ea79c272eb38419cabcb205f8cc0766
sonhakgyo <76437291+sonhakgyo@users.noreply.github.com>
2021-10-07 15:55:30 +0900

    update

---
+++ Test.java
@@ -0,0 +1,17 @@
+
+
+public class Test {
+    public static void main(String[] args) {
+        int a=100;
+        int result=0;
+
+        result=a >> 1;
+        result=result+1;
+        
+        result=a >> 2;
+        result=result+1;
+
+        System.out.println(result);
+        }
+    }
+
