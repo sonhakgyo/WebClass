97865521df1b96e9d07910466bd1f4d172a49ab0
sonhakgyo <76437291+sonhakgyo@users.noreply.github.com>
2021-10-11 06:22:00 +0900

    update

---
+++ JavaStudy/Exception/Try2.java
@@ -0,0 +1,18 @@
+package JavaStudy.Exception;
+
+public class Try2 {
+    public static void main(String[] args) {
+        int[] arData = new int[5];
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+            try {
+                arData[i] = i + 1;
+            } catch (Exception e) {
+                /* try문이 6번쨰까지 실행된다. 막을 방법이 있을까. */
+                break;
+            }
+        }
+        System.out.println("프로그램 정상 종료");
+    }
+}
