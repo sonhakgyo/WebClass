63871abaacfa6129615346604a503d9c379b3b5b
sonhakgyo <76437291+sonhakgyo@users.noreply.github.com>
2021-10-30 03:39:37 +0900

    update

---
+++ JavaScript_folder/류정원선생님/day05/script.js
@@ -0,0 +1,15 @@
+function plus(num1, num2) {
+    return num1 + num2;
+}
+
+function sub(num1, num2) {
+    return num1 - num2;
+}
+
+function mul(num1, num2) {
+    return num1 * num2;
+}
+
+function div(num1, num2) {
+    return num1 / num2;
+}
