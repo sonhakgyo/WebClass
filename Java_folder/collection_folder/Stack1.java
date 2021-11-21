83ac92d460271a5f7752cef826873975517db247
sonhakgyo <76437291+sonhakgyo@users.noreply.github.com>
2021-11-22 03:17:17 +0900

    UPDATE

--- Java_folder/collection_folder/Stack1.java
+++ Java_folder/collection_folder/Stack1.java
@@ -4,12 +4,13 @@ import java.util.Stack;
 
 public class Stack1 {
     public static void main(String[] args) {
+    //  스택 생성
         Stack<String> stack = new Stack<>();
+
     //  1. push() : 데이터 삽입
         stack.push("apple");
         stack.push("banana");
         stack.push("cherry");
-
         System.out.println(stack);
 
     //  2. pop() : top의 데이터 삭제
