2a42ec992c11cfd8f84c005ff93db68f2b044d3d
sonhakgyo <76437291+sonhakgyo@users.noreply.github.com>
2021-12-13 22:55:05 +0900

    UPDATE

--- JSP_folder/lsr/Connection1.java
+++ JSP_folder/lsr/Connection1.java
@@ -22,14 +22,20 @@ public class Connection1 { // 1을 빼면 생성자가 되어버린다.
 
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver"); // ClassNotFoundException
+        //  Class.forName(클래스명) : 클래스를 찾는다. 찾고난 후에 클래스를 로드한다.
+        //  클래스 로드 : 프로그램 시작 전에 프로그램이 필요한 해당 클래스를 메모리에 미리 준비하는 작업이다.
         //  Connection 인터페이스
             Connection con = (Connection) DriverManager.getConnection(url, id, password); 
+        //  Connection : 다리 역할의 객체(객체끼리 연결해준다. .java -- Oracle, DB)
             PreparedStatement ps = con.prepareStatement(sql_Create);
+
             ps.execute();
+            
             ps.close();
             con.close();
 
             System.out.println("시퀀스 생성 완료");
+
         } catch (ClassNotFoundException e) { // OracleDriver 클래스가 없을 때 발생한다.
             e.printStackTrace();
         } catch (SQLException e) { // 커넥션(DB연동)하는 과정에서 발생한다.
---
+++ JSP_folder/project1/MemberDAO.java
@@ -0,0 +1,29 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class MemberDAO {
+    private static final String driver = "oracle.jdbc.driver.OracleDriver";
+    private static final String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+    private static final String id = "user";
+    private static final String pw = "1234";
+
+    private Connection con;
+    private Statement stmt;
+    private ResultSet rs;
+
+    public ArrayList<MemberVO> list() {
+        ArrayList<MemberVO> list = new ArrayList<MemberVO>();
+        try {
+            conDB();
+            String query = "select * from Member";
+            System.out.println(query);
+            ResultSet rs = stmt.executeQuery(query);
+
+            while(rs.next() {
+                String id = rs.getString("id");
+            }
+        }
+    }
+}
