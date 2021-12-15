5fcfe746550a9f004f695aa52fbfac4cbb619038
sonhakgyo <76437291+sonhakgyo@users.noreply.github.com>
2021-12-16 06:27:28 +0900

    jh

--- JSP_folder/TestFromEcl.java
+++
@@ -1,7 +0,0 @@
-package JSP_folder;
-
-public class TestFromEcl {
-	public static void main(String[] args) {
-		System.out.println("연동 테스트2");
-	}
-}
---
+++ JSP_folder/test3.java
@@ -0,0 +1,16 @@
+package JSP_folder;
+
+public class test3 {
+	public static void main(String[] args) {
+		System.out.println("push to origin?");
+		System.out.println("실패...?");
+		
+		
+		System.out.println("push to branch main?");
+		
+		
+		System.out.println("push to origin?");
+		
+		
+	}
+}
