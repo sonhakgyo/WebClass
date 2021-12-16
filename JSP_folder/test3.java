14d3c20c9feef775ede17e93e3e2d891f08e5f45
sonhakgyo <76437291+sonhakgyo@users.noreply.github.com>
2021-12-16 12:39:51 +0900

    update

--- JSP_folder/test3.java
+++ JSP_folder/test3.java
@@ -2,6 +2,7 @@ package JSP_folder;
 
 public class test3 {
 	public static void main(String[] args) {
+	//  Push
 		System.out.println("push to origin?");
 		System.out.println("실패...?");
 		System.out.println("origin과 branch는 대체 무엇인가.");
@@ -11,9 +12,7 @@ public class test3 {
 		System.out.println("성공");
 		System.out.println("여기로 커밋하는 게 확실해보인다.");
 		
-		
-		System.out.println("push to ?");
-		
-		
+	//  Pull	
+		System.out.println("VSC 커밋 --> 깃허브 데스크탑 푸쉬 --> 이클립스 Team - Pull");
 	}
 }
